a3b4473b6d4053856c304c416eaeda17f1dab15e
OhadRubin <iohadrubin@gmail.com>
2023-03-16 14:13:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='halton',
+    name='haltonpy',
     version='0.1.0',
     author='Ohad Rubin',
     author_email='your.email@example.com',
     description='A short description of your library',
-    packages=["halton"],
+    packages=["haltonpy"],
     install_requires=[
         'numpy',
         'absl-py',
